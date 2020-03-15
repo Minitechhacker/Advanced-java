6ebc7e9d4c294c69aadf602a3ba7c3b4c765f351
Mini_tech_hacker <57276723+Minitechhacker@users.noreply.github.com>
2020-03-15 16:00:45 +0530

    Add files via upload

---
+++ StrutsEx/src/com/action/RegisterAction.java
@@ -0,0 +1,19 @@
+package com.action;
+
+public class RegisterAction
+{
+	private String uname;
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String execute()
+	{
+		return "success";
+	}
+
+}
